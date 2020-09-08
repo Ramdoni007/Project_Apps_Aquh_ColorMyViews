99a957cd0223e532ac02cee4370a0b78922e62b2
Ramdoni007 <ramd42156@gmail.com>
2020-09-08 15:09:53 +0700

    Penambahan materi Exercise: Add Baseline Constraint and Button Chain
     :)

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clicklableView:  List<View> =
-            listOf(box_one_text,box_two_text,box_three_text,box_four_text,box_five_text,constraints_layout)
+            listOf(box_one_text,box_two_text,box_three_text,box_four_text,box_five_text,constraints_layout,
+                red_button,green_button,yellow_button )
 
             for ( item in clicklableView ) {
                 item.setOnClickListener { makeColored (it) }
@@ -41,9 +42,12 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
+            // Box Menggunakan Warna Custom dari background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
-
-
 
 
         }
